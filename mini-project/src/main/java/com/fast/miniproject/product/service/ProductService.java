734525b832d2e2467e38345a72b2a1609498acd3
cutegyuseok <yigyuseok@gmail.com>
2023-02-14 19:42:55 +0900

    feat: 상품 구매 로직

--- mini-project/src/main/java/com/fast/miniproject/product/service/ProductService.java
+++ mini-project/src/main/java/com/fast/miniproject/product/service/ProductService.java
@@ -1,11 +1,17 @@
 package com.fast.miniproject.product.service;
 
+import com.fast.miniproject.auth.dto.LoginReqDTO;
 import com.fast.miniproject.global.response.ResponseDTO;
 import com.fast.miniproject.product.dto.ProductDTO;
+
+import java.util.ArrayList;
+
 public interface ProductService {
 
     ResponseDTO selectProductDetail(Long product_id);
     
     public ResponseDTO<?> selectProduct();
+
+    ResponseDTO buyProduct(ArrayList<Integer> products_id_list, LoginReqDTO user);
 
 }
